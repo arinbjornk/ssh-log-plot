d4a552a791ae3caaa2e6163630b995def9b184d7
arinbjorn <arinbjorn@gmail.com>
2017-09-17 15:04:19 +0000

    Put coordinates into list

--- matter.py
+++ matter.py
@@ -4,20 +4,26 @@ import urllib.request
 import collections
 import json
 
+# Get current external IP
 my_ip = urllib.request.urlopen('http://ident.me').read().decode('utf8')
 print(my_ip)
 
+# Get list of all IPs in authentication log
 with open('/var/log/auth.log', 'r') as f:
     output = f.read()
-ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', output)
+ip_log = re.findall(r'[0-9]+(?:\.[0-9]+){3}', output)
 
-firstcounter = collections.Counter(ip)
-counter = [[k, v] for k, v in firstcounter.items()]
-# print(counter)
-# print(counter[0][0])
+# Convert to counter to get frequencies of IPs
+counter = collections.Counter(ip_log)
+iplist = [[k, v] for k, v in counter.items()]
 
-for i in range(len(counter)):
-    url = 'http://ip-api.com/json/' + str(counter[i][0])
+# Get lat an lon of each IP
+coord = []
+for i in range(len(iplist)):
+    url = 'http://ip-api.com/json/' + str(iplist[i][0])
     response = urllib.request.urlopen(url).read().decode('utf-8')
     data = json.loads(response)
-    print(data['city'])
+    # print(data['lat'])
+    coord.append([data['lat'], data['lon']])
+
+print(coord)
